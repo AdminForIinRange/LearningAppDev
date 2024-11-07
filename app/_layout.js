00ae4b79784339bbbd9e0fd57e59c9d7e5e3ab30
Anjesh Bhattarai <91888685+AdminForIinRange@users.noreply.github.com>
2024-11-07 04:12:58 +0000

    build

---
+++ app/_layout.js
@@ -0,0 +1,8 @@
+import { Stack } from 'expo-router/stack';
+
+const Layout = () => {
+return <Stack />
+
+}
+
+export default Layout;
---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>test</Text>
+    </View>
+  );
+};
+
+
+export default Home
